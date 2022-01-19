dbb8d688b008f0881ab45a4df053220620d50042
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2022-01-19 12:42:52 +0700

    improve renderUserBlock function

--- src/user.ts
+++ src/user.ts
@@ -1,6 +1,10 @@
 import { renderBlock } from './lib.js'
 
-export function renderUserBlock (favoriteItemsAmount) {
+export function renderUserBlock (
+    userName:string = '',
+    avaLnk:string = '',
+    favoriteItemsAmount:number = 0
+  ) {
   const favoritesCaption = favoriteItemsAmount ? favoriteItemsAmount : 'ничего нет'
   const hasFavoriteItems = favoriteItemsAmount ? true : false
 
@@ -8,9 +12,9 @@ export function renderUserBlock (favoriteItemsAmount) {
     'user-block',
     `
     <div class="header-container">
-      <img class="avatar" src="/img/avatar.png" alt="Wade Warren" />
+      <img class="avatar" src="${avaLnk}" alt="Wade Warren" />
       <div class="info">
-          <p class="name">Wade Warren</p>
+          <p class="name">${userName}</p>
           <p class="fav">
             <i class="heart-icon${hasFavoriteItems ? ' active' : ''}"></i>${favoritesCaption}
           </p>
